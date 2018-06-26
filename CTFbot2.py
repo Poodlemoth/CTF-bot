5aa00edcef697ada877f1270027bacdfb1e00bfc
Thomas <Suitengo@gmail.com>
2018-06-25 22:51:22 -0700

    fixed mistakes changed hello message to new api

--- CTFbot2.py
+++ CTFbot2.py
@@ -1,21 +1,19 @@
-from discord.ext.commands import Bot
-from discord import Game
+import discord
 import random
 import time
 import requests
 
 BOT_PREFIX = ("!","?")  
-TOKEN = 'Mjg1ODU0MDk4OTcwOTAyNTI5.DhMEqA.5hpwdiHc0jkOgn2gm4I_KpoRzA0'
+TOKEN = {PUT TOKEN HERE}
 client = Bot(command_prefix=BOT_PREFIX)
 UTIME = time.time()
 
-@client.command()
-async def Hello():
-    responces = [
-        'Hello, How are you?',
-        'Hello'
-    ]
-    await client.say(random.choice(responces))
+@client.event()
+async def on_message():
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send('$Hello!')
 
 @client.command()
 async def CTF():
