39f042d082aafdd1f50635d5f768cbe8328b86d0
Thomas <Suitengo@gmail.com>
2018-06-25 23:14:35 -0700

    finished changing to new api

--- CTFbot2.py
+++ CTFbot2.py
@@ -3,25 +3,29 @@ import random
 import time
 import requests
 
-BOT_PREFIX = ("!","?")  
+ 
 TOKEN = {PUT TOKEN HERE}
-client = Bot(command_prefix=BOT_PREFIX)
 UTIME = time.time()
+client = discord.Client()
 
-@client.event()
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('!hello'):
+        await message.channel.send('$Hello!')
+
+@client.event
 async def on_message():
     if message.author == client.user:
         return
-    if message.content.startswith('$hello'):
-        await message.channel.send('$Hello!')
+    if message.content.startswith('$CTF'):
+        url ='https://ctftime.org/api/v1/events/?start='+str(UTIME)+'BTC.json'
 
-@client.command()
-async def CTF():
-    url ='https://ctftime.org/api/v1/events/?start='+str(UTIME)+'BTC.json'
-
-    responce = requests.get(url)
-    value = responce.json() ['url']
-    await client.say ("Current events are... " + value)
+        responce = requests.get(url)
+        value = responce.json() ['url']
+        await client.channel.send("$Current events are... " + value)
 
 
 
