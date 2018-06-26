59a238148c11336c98719250d472ba7b63b71e55
2sodiumsandwich <hieu0218@gmail.com>
2018-06-26 11:13:25 -0700

    changed client to bot and rewrote how commands were being seen, plus latency command

--- CTFbot.py
+++ CTFbot.py
@@ -1,40 +1,41 @@
 from discord.ext import commands
-import discord
 import requests
 import json
-
-
-client = discord.Client() 
 
 with open("./auth.json", "r") as read_file:
 	data = json.load(read_file)
 TOKEN = data.get("token")
-prefix = data.get("prefix")
+#prefix = data.get("prefix")
 
+prefix = "$"
+bot = commands.Bot(command_prefix='$')
 
-@client.event
+@bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(bot))
 
-@client.event
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+	print(message.content)
+	await bot.process_commands(message)
 
 
-client.run(TOKEN)
 
-@client.event
-async def on_message():
-    if message.author == client.user:
-        return
-    if message.content.startswith('$CTF'):
-        url ='https://ctftime.org/api/v1/events/?start='+str(UTIME)+'BTC.json'
+@bot.command()
+async def ping(ctx):
+	await ctx.send(bot.latency)
 
-        responce = requests.get(url)
-        value = responce.json() ['url']
-        await client.channel.send("$Current events are... " + value)
 
+@bot.command()
+async def hello(ctx):
+	await ctx.send("Hello!")
+
+@bot.command()
+async def ctf(ctx):
+	url ='https://ctftime.org/api/v1/events/?start='+str(UTIME)+'BTC.json'
+
+	responce = requests.get(url)
+	value = responce.json() ['url']
+	await bot.channel.send("Current events are... " + value)
+
+bot.run(TOKEN)				
